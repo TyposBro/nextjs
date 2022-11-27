aaded9ff0552538e6374cef00f3d82cefce93e05
TyposBro <khasanjonovich@gmail.com>
2022-11-28 02:51:55 +0900

    Add Optimized Image

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,9 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function FirstPost() {
   return (
     <>
+    <Head>
+      <title></title>
+    </Head>
       <h1>First ever post</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
